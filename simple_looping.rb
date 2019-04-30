e54c5023c828c11a6c15d8964bfe60005cea217a
Jooyon Won <jyon1204@gmail.com>
2019-04-30 02:20:22 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,9 +1,14 @@
 # REMEMBER: print your output to the terminal using 'puts'
 
 def loop_iterator(number_of_times)
-
+number_of_times = 0;
+loop do
+  number_of_times = number_of_times + 1;
   phrase = "Welcome to Flatiron School's Web Development Course!"
-
+  puts phrase
+  if number_of_times == 7
+    break
+  end
 end
 
 def times_iterator(number_of_times)
