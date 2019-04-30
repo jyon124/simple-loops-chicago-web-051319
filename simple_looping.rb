737b007860a9c35811b177844b09087640057add
Jooyon Won <jyon1204@gmail.com>
2019-04-30 02:21:58 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -5,7 +5,7 @@ number_of_times = 0;
 loop do
   number_of_times = number_of_times + 1
   puts "Welcome to Flatiron School's Web Development Course!"
-  if number_of_times == 7
+  if number_of_times > 6
     break
   end
 end
